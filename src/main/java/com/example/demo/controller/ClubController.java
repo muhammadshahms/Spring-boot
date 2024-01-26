67fa69efb195ffe282bbc4353600240574cb0d5a
muhammadshahms <muhammadshah4589@gmail.com>
2024-01-26 11:46:20 +0500

    day 5

--- src/main/java/com/example/demo/controller/ClubController.java
+++ src/main/java/com/example/demo/controller/ClubController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.dto.ClubDto;
 import com.example.demo.models.Club;
 import com.example.demo.services.ClubService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Controller
@@ -17,6 +20,11 @@ public class ClubController {
     @Autowired
     public ClubController(ClubService clubService) {
         this.clubService = clubService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 
     @GetMapping("/club")
@@ -49,7 +57,14 @@ public class ClubController {
     }
 
     @PostMapping("/club/{clubId}/edit")
-    public String updateClub(@PathVariable("clubId") Long clubId, @ModelAttribute("club") ClubDto club) {
+    public String updateClub(@PathVariable("clubId") Long clubId,
+                             @Valid @ModelAttribute("club") ClubDto club,
+                             BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "club-edit";
+        }
+
         club.setId(clubId);
         clubService.updateClub(club);
         return "redirect:/club";
--- src/main/java/com/example/demo/dto/ClubDto.java
+++ src/main/java/com/example/demo/dto/ClubDto.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +10,11 @@ import java.time.LocalDateTime;
 @Builder
 public class ClubDto {
     private Long id;
+    @NotEmpty(message = "Title cannot be empty")
     private String title;
+    @NotEmpty(message = "Photo URL cannot be empty")
     private String photoUrl;
+    @NotEmpty(message = "Content cannot be empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
